3d3d8ee417ea5cf918dc47c1f3fd8cdec375fd98
nuriafs <nf@MacBook-Air-NF.local>
2023-06-22 20:14:27 +0200

    Funcionalidad del juego terminada.

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,103 @@
 "use strict";
 
 //functions con export-import al main
+
+// Creamos una función para revelar la carta y aplicar flipped
+function reveal(event) {
+    if (lockBoard) return; //
+
+    let card = event.currentTarget; // carta = click en el article
+    card.classList.add("flipped"); //le añadimos la clase flipped si:
+    if (card === firstCard) return;
+    // comparamos las variables de las cartas
+    if (!firstCard) {
+        firstCard = card;
+        return
+        //si firstcard no tiene valor asignado (es decir es null) entonces se le asigna el valor de la carta atual (card) a la variable
+    }
+    secondCard = card;
+    //si secondcard no tiene valor asignado (es decir es null) entonces se le asigna el valor de la carta atual (card) a la variable
+    attempts++; //+1 intento cuando cliquemos dos cartas
+    score.textContent = attempts; //actualiza el contador
+    lockBoard = true;
+    checkForMatch();
+
+}
+
+
+//*dataset use:
+//For example, a data-abc-def attribute (in index) corresponds to dataset.abcDef. So in this case data-name (in index) corresponds to dataset.name in JS
+
+
+
+//contador que aumente en uno cuando dos cartas se revelan (done)
+
+// reset button : attempts -->0 / cartas reverso / suffle (done)
+
+function reset() {
+    attempts = 0;
+    resetValues();
+    suffledArray(emojisArray);
+    score.textContent = attempts;
+    mainElement.innerHTML = ""; // borra la visualizacion de las cartas antes de generarlas de nuevo en el browser. Así lograremos tener siempre SOLO 16 cartas en el browser.
+    generateCards();
+}
+
+// función para que las cartas se barajen. Es un función que se utiliza mucho. // de atrás hacia adelante va cambiando las posiciones de los elementos dentro del array aleatoriamente
+function suffledArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        const randomItem = array[randomIndex];
+        array[randomIndex] = array[i];
+        array[i] = randomItem;
+    }
+    return array;
+}
+
+function generateCards() {
+    //llamamos al main
+
+    for (const objects of emojisArray) {
+        //crear el elemento article
+        const articleElement = document.createElement('article');
+        //añadir la class=card
+        articleElement.classList.add("card");
+        articleElement.innerHTML = `
+      <section class="front" data-name=${objects.id}> ${objects.emoji} </section>
+      <section class="back"></section>
+      `;
+        //(meterlo dentro del main) appendChild
+        mainElement.appendChild(articleElement);
+        articleElement.addEventListener("click", reveal);
+    }
+}
+
+//Esta función se encarga de quitarles el evento a las dos cartas que ya descubrimos siendo iguales
+function disableCards() {
+    firstCard.removeEventListener("click", reveal);
+    secondCard.removeEventListener("click", reveal);
+    resetValues();
+}
+
+//Se encarga de quitarle la clase flipped a las dos cartas giradas y que NO son iguales. Y devuelve los valores de las variables globales a su estado inicial: null.
+function unReveal() {
+    setTimeout(() => {
+        firstCard.classList.remove("flipped");
+        secondCard.classList.remove("flipped");
+        resetValues();
+    }, 1000);
+}
+
+//Se encarga de comparar los valores de dataset que le asignamos a las cartas y si no son iguales las vuelve a girar
+function checkForMatch() {
+    let isMatch = firstCard.querySelector(".front").dataset.name ===
+        secondCard.querySelector(".front").dataset.name;
+    isMatch ? disableCards() : unReveal();
+}
+
+//Se encarga de reestablecer las variables globales a su estado inicial.
+function resetValues() {
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
+}
--- js/main.js
+++ js/main.js
@@ -1,117 +1,26 @@
 "use strict";
 import { emojisArray } from "./data.js";
 
-//Encontrar el value del data-name(string)
-
-const articleElement = document.querySelectorAll("article");
-const score = document.querySelector(".score");
+//VARIABLES GLOBALES
 let firstCard = null; // null porque  el valor aún no esta asignado
 let secondCard = null;
 let attempts = 0;
+let lockBoard = false; // 
+
+// teniamos un problema de que podiamos clicar dos veces a la primera carta, dandole así el valor de la primera carta a la segunda también y ya no seguia el juego. Tambien se podian clicar mas de dos cartas a la vez.
+
+
+//MANIPULACIÓN DEL DOM
+const score = document.querySelector(".score");
+const resetBtn = document.querySelector('#resetBtn');
+const mainElement = document.querySelector("#container")
+
+resetBtn.addEventListener("click", reset);
+
 score.textContent = attempts; //inicializamos el contador
 
-// recorremos el array articleElemnt para aplicar el evento click
-for (let card of articleElement) {
-  card.addEventListener("click", reveal);
-  //*crear loop que recorra cada uno de los elementos del array
-  //crear las section y articles dinamicamente
-}
-// Creamos una función para revelar la carta y aplicar flipped
-function reveal(event) {
-  let card = event.currentTarget; // carta = click en el article
-  card.classList.add("flipped"); //le añadimos la clase flipped si:
-  // comparamos las variables de las cartas
-  if (!firstCard) {
-    firstCard = card;
-    //si firstcard no tiene valor asignado (es decir es null) entonces se le asigna el valor de la carta atual (card) a la variable
-  } else if (!secondCard) {
-    secondCard = card;
-    //si secondcard no tiene valor asignado (es decir es null) entonces se le asigna el valor de la carta atual (card) a la variable
-    attempts++; //+1 intento cuando cliquemos dos cartas
-    score.textContent = attempts; //actualiza el contador
-    if (
-      //cuando tiene el valor de la segunda lo compara con el valor de la primera
-      firstCard.querySelector(".front").dataset.name ===
-      secondCard.querySelector(".front").dataset.name
-    ) {
-      //console.log("Las cartas son iguales");
-      //si son iguales se restablecen los valores de las variables para poder seleccionar 2 cartas nuevas
 
-      firstCard = null;
-      secondCard = null;
-    } else {
-      //console.log("Las cartas no son iguales");
-      // cuando no son iguales se quita la clase flipped (las cartas se giran) en 1sec
-      setTimeout(() => {
-        firstCard.classList.remove("flipped");
-        secondCard.classList.remove("flipped");
-        firstCard = null;
-        secondCard = null;
-      }, 1000);
-    }
-  }
-}
+suffledArray(emojisArray);
+generateCards();
 
-//contador que aumente en uno cuando dos cartas se revelan (done)
 
-// reset button : attempts -->0 / cartas reverso / suffle (luego)
-
-/* cosas que nos faltan:
-  - boton start
-  - array js con emojis (hecho)
-  - función Math.random para barajar las cartas 
-  - modificar el juego ya creado para que llame a los emojis
-  - separar lo que tenemos hecho
-
-*/
-
-//For example, a data-abc-def attribute = data-name(del index) corresponds to dataset.abcDef = dataset.name
-
-// const articleElement = document.querySelectorAll("article");
-// console.log(articleElement);
-
-// lograr que giren dos cartas (feito)
-// for (let card of articleElement) {
-//   card.addEventListener("click", reveal);
-// es algo que tenemos que implementar
-
-//   console.log(card);
-//   if (card1 !== card2) {
-//     setTimeout(() => {
-//       card.classList.remove("flipped");
-//     }, 1000);
-//   }
-//}
-
-// lograr seleccionar las cartas.
-
-// lograr que las cartas se vuelvan a girar(feitiño)
-//funcion que de la vuelta a la carta (article) y a los 2sec la vuelva a girar
-// function reveal(event) {
-//   const currentCard = event.currentTarget;
-//   currentCard.classList.add("flipped");
-// }
-
-// const button = document.querySelector("#resetBtn")
-
-// another option
-
-// let cards = document.querySelectorAll(".card");
-//   for (let i = 0; i < cards.length; i++) {
-//     for (let j = i + 1; j < cards.length; j++) {
-//       if (
-//         cards[i].querySelector(".front").dataset.name ===
-//         cards[j].querySelector(".front").dataset.name
-//       ) {
-//         cards[i].classList.add("flipped");
-//         cards[j].classList.add("flipped");
-//         //console.log("Las cartas son iguales");
-//       } else {
-//         //console.log("Las cartas no son iguales");
-//         setTimeout(() => {
-//           cards[i].classList.remove("flipped");
-//           cards[j].classList.remove("flipped");
-//         }, 1000);
-//       }
-//     }
-//   }
